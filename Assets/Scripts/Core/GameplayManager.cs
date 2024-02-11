c193a206d82bf193b2150dadac6ae94b53e94f31
Spyridon Passas <spyridonpassas0325@gmail.com>
2024-02-12 00:42:54 +0100

    Started on procedual level generation (WFC)

---
+++ Assets/Scripts/Core/GameplayManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+    enum GameplayState
+    {
+        Intro,
+        MainMenu,
+        InGame,
+        Paused
+    }
+
+    private GameplayState currentState;
+
+    private void Awake()
+    {
+        currentState = GameplayState.Intro;
+    }
+
+
+
+}
+
---
+++ Assets/Scripts/Core/LevelManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public struct Tile
+{
+    public int prefabID;
+}
+
+public class LevelManager : MonoBehaviour
+{
+    private static LevelManager instance;
+
+    public int tileWidth;
+    public int tileHeight;
+
+    public List<TilePreset> tilePresets;
+
+    private Tile[] grid;
+
+    private void Awake()
+    {
+        instance = this;
+        grid = new Tile[tileWidth * tileHeight];
+    }
+
+
+    public static void LoadNewLevel()
+    {
+        instance._LoadNewLevel();
+    }
+
+    private void _LoadNewLevel()
+    {
+        while (!IsCollapsed())
+            Iterate();
+    }
+
+    private void Iterate()
+    {
+        var coords = GetMinEntropyCoordinates();
+        CollapseAt(coords);
+        Propagate(coords);
+    }
+
+    private bool IsCollapsed()
+    {
+
+    }
+}
+
